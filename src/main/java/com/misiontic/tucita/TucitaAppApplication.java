7e5c1463a4f5598824ceed893a084f31e8dc1291
Yelan <>
2022-08-31 16:22:26 -0500

    Primer Carga Proyecto

---
+++ src/main/java/com/misiontic/tucita/TucitaAppApplication.java
@@ -0,0 +1,13 @@
+package com.misiontic.tucita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TucitaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TucitaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misiontic/tucita/TucitaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misiontic.tucita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TucitaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
